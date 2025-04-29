685f9af866c82dfe1bf9f8fc07338fa289ea2855
shehanminura <shehanminuradp@gmail.com>
2025-04-29 16:16:39 +0530

    add http

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,10 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideHttpClient } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 
-import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
-
 export const appConfig: ApplicationConfig = {
-  providers: [provideHttpClient(),
-    FormsModule,provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay())]
+  providers: [
+    provideHttpClient(),
+    importProvidersFrom(FormsModule)
+  ]
 };
